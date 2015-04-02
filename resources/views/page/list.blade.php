954eed9a17c6649dd57e5af4675aa41d42734789
John Dapzar <johndapzar@yahoo.com>
2015-04-02 21:51:22 +0530

    Download

--- resources/views/page/list.blade.php
+++ resources/views/page/list.blade.php
@@ -8,7 +8,14 @@
 	</div>
 	<ol start='1'>
 	@foreach($postByCat as $post)
-		<li><a href="{{ URL::route('page.index','id='.$post->id) }}">{{ $post->title }}</a></li>
+		<?php 
+			if(strlen($post->download)>3 && strlen($post->body)<1) {
+				echo "<li><a href='$post->download' download>$post->title</a></li>";
+			} else {
+				echo "<li><a href=" . URL::route('page.index','id='.$post->id) . ">$post->title</a></li>";
+			}
+		?>
+		
 	@endforeach
 	<ol>
 	{!! $postByCat !!}
--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -7,6 +7,10 @@
 		<h3>{{ strtoupper($page->title) }}</h3>
 	</div>
 	{!! $page->body !!}
+
+	@if(strlen($page->download)>3)
+		<br> <span class="glyphicon glyphicon-paperclip"></span>&nbsp;&nbsp; <a href="{{ $page->download }}" download>Download File Attachment</a>
+	@endif
 @endif
 
 @endsection
